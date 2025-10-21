26996523f2dfd0ea52183cf2542feff03ec2d5af
Julian Enrique <julianbuitragocharry2006@gmail.com>
2025-10-20 23:26:56 -0500

    deleted node_modules

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-echo "🔧 Generando Prisma client..."
-npm run prisma:generate
-
-echo "📦 Ejecutando migraciones..."
-npm run prisma:migrate
-
-echo "🚀 Iniciando servidor..."
-npm run start
